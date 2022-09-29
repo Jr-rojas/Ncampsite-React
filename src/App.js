3cc339e1a1a6c8733269ae4f8cc3d48e8a5791f6
Jersain Rojas <j.rojas_7@hotmail.com>
2022-09-28 17:54:59 -0700

    useState

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import CampsitesList from './features/campsites/CampsitesList';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,13 @@
+import {CAMPSITES} from '../../app/shared/CAMPSITES';
+
+export const selectAllCampsites = () => {
+    return CAMPSITES
+};
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
+};
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(CAMPSITES.length* Math.random())];
+// };
+
