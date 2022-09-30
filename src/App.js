a0aa0c3292b731181432d3cbf2a828e3cc4198e4
Jersain Rojas <j.rojas_7@hotmail.com>
2022-09-29 19:56:46 -0700

    CampsiteDetailPage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
 
 function App() {
@@ -16,6 +16,10 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route
+            path='directory/:campsiteId'
+            element={<CampsiteDetailPage/>}
+        />
       </Routes>
       <Footer />
     </div>
--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,16 +1,20 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-const CampsiteCard = (props) => {
+const CampsiteCard = ({ campsite }) => {
+    const { id, image, name} = campsite;
     return(
-        <Card>
-            <CardImg
-                width='100%' src={props.campsite.image} alt={props.campsite.name}
-             />
-             <CardImgOverlay>
-                <CardTitle>{props.campsite.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
+        <Link to={`${id}`}>
+            <Card>
+                <CardImg
+                    width='100%' src={image} alt={name}
+                />
+                <CardImgOverlay>
+                    <CardTitle>{name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        </Link>
     );
-}
+};
 
 export default CampsiteCard;
