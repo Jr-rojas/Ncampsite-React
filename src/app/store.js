5be390c5f24ae74341347136b8dba7a81c7fb2a5
Jersain Rojas <j.rojas_7@hotmail.com>
2022-10-15 12:14:02 -0700

    workshop4

--- src/app/store.js
+++ src/app/store.js
@@ -4,13 +4,15 @@ import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
     comments: commentsReducer,
     partners: partnersReducer,
-    promotions: promotionsReducer
+    promotions: promotionsReducer,
+    user: userReducer
   },
   //middlewareProperty 
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
