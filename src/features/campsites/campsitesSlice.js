e5081c6bd20ff1e1f8d1927764e4b7cca25cd697
Jersain Rojas <j.rojas_7@hotmail.com>
2022-10-20 21:14:18 -0700

    Error and Loading

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,16 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
-import {CAMPSITES} from '../../app/shared/CAMPSITES';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+//import {CAMPSITES} from '../../app/shared/CAMPSITES';
+import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImageURL } from '../../utils/mapImageURL';
 
+export const fetchCampsites = createAsyncThunk(
+    'campsites/fetchCampsites',
+    async () => {
+        const response = await fetch(baseUrl + 'campsites');
+        if (!response.ok) {
+            return Promise.reject('Unable to fetch , status: ' + response.status)
+        }
+        const data = await response.json();
+        return data;
+    }
+)
 //The initial state is the array of campsites from the local campsites file.
-const initialState = {
+/* const initialState = {
     campsitesArray: CAMPSITES
+}; */
+const initialState = {
+    campsitesArray: [],
+    isLoading: true,
+    errMsg: ''
 };
 
 // the value is a function with 2 properties 'name' with the key of 'campsites' and initialState.
 //This will return an object as a value.
 const campsiteSlice = createSlice({
     name: 'campsites',
-    initialState
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [fetchCampsites.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [fetchCampsites.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = '';
+            state.campsitesArray = mapImageURL(action.payload)
+        },
+        [fetchCampsites.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg=action.error ? action.error.message : 'Fetch failed';
+        }
+    }
 });
 
 //export it as a function 
